80d3d00e86feacfd44a985baa7938e0ec7181e54
Daria <pitosha13@gmail.com>
2026-04-01 17:46:26 +0300

    add styles in Bookings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // webpack(config) {
+  //   // 1. Находим правило для SVG в стандартном loader'e
+  //   const fileLoaderRule = config.module.rules.find((rule: any) =>
+  //     rule.test?.test?.(".svg"),
+  //   );
+  //   // 2. Исключаем svg из стандартного file-loader
+  //   if (fileLoaderRule) {
+  //     fileLoaderRule.exclude = /\.svg$/;
+  //   }
+  //   // 3. Добавляем SVGR
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     issuer: /\.[jt]sx?$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+  //   return config;
+  // },
 };
 
 export default nextConfig;
